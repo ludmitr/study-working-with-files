823ae263cd8db25904599001f0ae1fa3428a12a3
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-03-29 16:25:48 +0300

    finish

--- organize_photos.py
+++ organize_photos.py
@@ -1,25 +1,30 @@
 import os
+import shutil
 
 
 def main():
-    current_folder = os.getcwd()
-    photos_folder = current_folder + "\Photos"
+    current_folder_path = os.getcwd()
+    photos_folder_path = current_folder_path + "\Photos"
 
     # creating list of cities for places photos been taken
-
-    file_names = get_file_names(photos_folder)
+    file_names = get_file_names(photos_folder_path)
     cities = [f_name.split("_", 2)[1] for f_name in file_names]
-    cities = list(set(cities)) # removing_duplicates
+    cities_without_duplicates = list(set(cities)) # removing_duplicates
 
-    create_new_folders(current_folder, cities)
+    create_new_folders(current_folder_path,
+                       cities_without_duplicates)
 
-
-if __name__ == '__main__':
-    main()
+    # moving photos from folder Photos to folders inside project with
+    # matching city name
+    for i in range(len(file_names)):
+        file_name = f"\\{file_names[i]}"
+        old_path = photos_folder_path + file_name
+        new_path = current_folder_path + "\\" + cities[i] + file_name
+        move_file(old_path, new_path)
 
 
 def move_file(old_path, new_path):
-    pass
+    shutil.move(old_path, new_path)
 
 
 def get_file_names(path: str) -> list:
@@ -34,3 +39,7 @@ def create_new_folders(path, folder_names):
         final_directory = os.path.join(path, folder)
         if not os.path.exists(final_directory):
             os.makedirs(final_directory)
+
+
+if __name__ == '__main__':
+    main()
