b9c9f98ba09a96e37c13042cb9631814187eedf9
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-03-30 06:02:54 +0300

    udacity code

--- organize_photos.py
+++ organize_photos.py
@@ -1,43 +1,14 @@
 import os
 
-
 def main():
-    current_folder_path = os.getcwd()
-    photos_folder_path = current_folder_path + "\Photos"
-
-    # creating list of cities for places photos been taken
-    file_names = get_file_names(photos_folder_path)
-    cities = [f_name.split("_", 2)[1] for f_name in file_names]
-    cities_without_duplicates = list(set(cities)) # removing_duplicates
-
-    create_new_folders(current_folder_path,
-                       cities_without_duplicates)
-
-    # moving photos from folder Photos to folders inside project with
-    # matching city name
-    for i in range(len(file_names)):
-        file_name = f"\\{file_names[i]}"
-        old_path = photos_folder_path + file_name
-        new_path = current_folder_path + "\\" + cities[i] + file_name
-        move_file(old_path, new_path)
+    os.chdir("Photos")
+    originals = os.listdir()
+    places = [extract_place(file_name) for file_name in originals]
 
 
-def move_file(old_path, new_path):
-    os.replace(old_path, new_path) # if the file exist it will replace it. rename will raise am error
-
-
-def get_file_names(path: str) -> list:
-    """create and return list with all files names in path"""
-    files = [f for f in os.listdir(path)
-             if os.path.isfile(os.path.join(path, f))]
-    return files
-
-
-def create_new_folders(path, folder_names):
-    for folder in folder_names:
-        final_directory = os.path.join(path, folder)
-        if not os.path.exists(final_directory):
-            os.makedirs(final_directory)
+def extract_place(file_name: str) -> str:
+    """Extracts the place from a filename and returns it"""
+    return file_name.split("_")[1]
 
 
 if __name__ == '__main__':
